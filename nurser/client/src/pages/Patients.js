18f4d9248a54a08e8773ca735d182dcb7e655022
desivar <jillian_desire@yahoo.es>
2025-06-28 14:34:34 -0600

    add Patients

---
+++ nurser/client/src/pages/Patients.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Patients() {
+  return (
+    <div className="patients">
+      <h1>Patient Management</h1>
+      {/* Add patient management content */}
+    </div>
+  );
+}
