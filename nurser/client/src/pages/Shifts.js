1220d67ecbac2bf597c570e8c7b903b88d6d320f
desivar <jillian_desire@yahoo.es>
2025-06-28 14:35:20 -0600

    add pages

---
+++ nurser/client/src/pages/Shifts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Shifts() {
+  return (
+    <div className="shifts">
+      <h1>Shift Scheduling</h1>
+      {/* Add shift management content */}
+    </div>
+  );
+}
