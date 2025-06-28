3530130cdd0ef0a7b99e75ee598844451f58ef36
desivar <jillian_desire@yahoo.es>
2025-06-28 06:11:00 -0600

    add services/api.js

---
+++ nurser/client/src/services/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { useAuth } from '../context/AuthContext';
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+// Add a request interceptor to include the auth token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['x-auth-token'] = token;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
