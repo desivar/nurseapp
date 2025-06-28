e0a85a60871f7989b90b98fa8af1261f80f1f563
desivar <jillian_desire@yahoo.es>
2025-06-28 14:36:04 -0600

    add pages

---
+++ nurser/client/src/pages/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <h1>Your Profile</h1>
+      {/* Add profile content */}
+    </div>
+  );
+}
