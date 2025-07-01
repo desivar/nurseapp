58fd6bc58ee74e709ea75af40cd07b504b5a1fbc
desivar <jillian_desire@yahoo.es>
2025-07-01 16:58:45 -0600

    webpack

---
+++ nurser/client/src/config/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+
+module.exports = function override(config) {
+  config.resolve = {
+    ...config.resolve,
+    fallback: {
+      "fs": false,
+      "path": require.resolve("path-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "buffer": require.resolve("buffer/"),
+      "process": require.resolve("process/browser")
+    }
+  };
+
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ];
+
+  return config;
+};
