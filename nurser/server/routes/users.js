a1f65a75dfb104a92a597a94ebb14174a13292a7
desivar <jillian_desire@yahoo.es>
2025-06-30 18:19:16 -0600

    add users

---
+++ nurser/server/routes/users.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const authMiddleware = require('../middleware/auth');
+
+/**
+ * @swagger
+ * /users/me:
+ *   get:
+ *     summary: Get current user info
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Current user data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       401:
+ *         description: Unauthorized
+ */
+router.get('/me', authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
