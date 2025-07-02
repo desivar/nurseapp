a1a977fee1a4abd9c269bcdd4bd4fefff6a45b92
desivar <jillian_desire@yahoo.es>
2025-07-01 18:03:02 -0600

    package

--- nurser/client/config-overrides.js
+++ nurser/client/config-overrides.js
@@ -1,10 +1,21 @@
-const path = require('path');
+const webpack = require('webpack');
 
 module.exports = function override(config) {
   config.resolve.fallback = {
+    os: require.resolve('os-browserify/browser'),
     path: require.resolve('path-browserify'),
-    fs: false,
-    crypto: false
+    process: require.resolve('process/browser'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer/'),
+    util: require.resolve('util/')
   };
+  
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  );
+  
   return config;
 };
